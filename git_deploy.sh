d4496d2f7020802bdd489cd6eec59185fddd2dd6
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-12 10:19:55 -0700

    add circle-ci deploy script, attempt deployment on circleci back to github

---
+++ git_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+{
+  git pull
+} > /dev/null 2>&1
+
+git config --global user.email 'myrmecocystus@gmail.com' && git config --global user.name 'Scott Chamberlain'
+echo $(date) > date.txt
+git add date.txt
+git commit -m 'built and deployed from circle [ci skip]'
+{
+  git push https://sckott:$GH_TOKEN@github.com/sckott/msrgbif gh-pages
+} &> /dev/null 2>&1
